442bdaddbe0f006fb50da82e90b5215fc5114cc8
James Prince <james.prince1@gmail.com>
2023-01-18 14:09:22 +1100

    Added createUser and login resolvers

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,4 +1,4 @@
-import { AuthenticationError } from "apollo-server-express";
+import { AuthenticationError, ValidationError } from "apollo-server-express";
 import { Book, User } from "../models";
 import { signToken } from "../utils/auth";
 
@@ -6,15 +6,49 @@ export const resolvers = {
     Query: {
         getSingleUser: async () => {
 
+
         },
 
     },
     Mutation: {
-        createUser: async () => {
+        createUser: async (parent, { username, email, password }) => {
+            if (!username || !email || !password) {
+                throw new ValidationError("Please provide a Username, Email and Password");
+            }
+            const user = await User.create({
+                username,
+                email,
+                password,
+                savedBooks: []
+            })
 
+            const token = signToken({
+                username: user.username,
+                email: user.email,
+                _id: user._id
+            })
+
+            return { token, user };
         },
 
-        login: async () => {
+        login: async (parent, { email, password }) => {
+            const user = User.findOne({ email });
+
+            if(!user){
+                throw new AuthenticationError("Invalid Credentials");
+            }
+
+            if(!user.isCorrectPassword(password)){
+                throw new AuthenticationError("Invalid Credentials");
+            }
+
+            const token = signToken({
+                username: user.username,
+                email: user.email,
+                _id: user._id,
+            });
+
+            return { token, user };
 
         },
 
@@ -23,7 +57,7 @@ export const resolvers = {
         },
 
         deleteBook: async () => {
-            
+
         }
     }
 }
